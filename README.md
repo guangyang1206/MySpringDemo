02ef71a576eff860aa36aa7ef2dc46b75c57bfce
Guang Yang <Guang_Yang@epam.com>
2017-01-17 15:27:21 +0800

    init settings

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # MySpringDemo
-# MySpringDemo
+
---
+++ src/main/java/com/epam/java/mentoring/MySpringDemoApplication.java
@@ -0,0 +1,12 @@
+package com.epam.java.mentoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringDemoApplication.class, args);
+	}
+}
